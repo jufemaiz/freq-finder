907e8efe75d8842c765502e9208d97629f5bd96c
Joel Courtney <euphemize@gmail.com>
2011-07-19 14:45:06 +1000

    update to models

---
+++ db/migrate/20110719044330_distance_column_removal.rb
@@ -0,0 +1,9 @@
+class DistanceColumnRemoval < ActiveRecord::Migration
+  def self.up
+    remove_column :transmitters, :distance
+  end
+
+  def self.down
+    add_column :transmitters, :distance
+  end
+end
