9027e28ed703d6bc91c5d785f67977c19d1ff17a
Joel Courtney <euphemize+git@gmail.com>
2018-12-22 22:26:40 +1100

    transmitters loop

--- app/lib/extract_transform_load.rb
+++ app/lib/extract_transform_load.rb
@@ -41,41 +41,43 @@ class ExtractTransformLoad
 
         station = Station.find_or_create_by(title: station_data[:title])
 
-        t = Transmitter.find_or_initialize_by(
-          band: transmitter[:band],
-          area: transmitter[:area],
-          callsign: transmitter[:callsign],
-          frequency: transmitter[:frequency],
-          purpose: transmitter[:purpose],
-          polarisation: transmitter[:polarisation],
-          antenna_height: transmitter[:antenna_height],
-          antenna_pattern: transmitter[:antenna_pattern],
-          maximum_erp: transmitter[:maximum_erp],
-          maximum_cmf: transmitter[:maximum_cmf],
-          power: transmitter[:power],
-          technical_specification_number: transmitter[:technical_specification_number],
-          license_number: transmitter[:license_number],
-          site_id: transmitter[:site_id],
-          site_name: transmitter[:site_name],
-          zone: transmitter[:zone],
-          easting: transmitter[:easting],
-          northing: transmitter[:northing],
-          lat: transmitter[:lat],
-          lng: transmitter[:lng],
-          state: transmitter[:state],
-          bsl: transmitter[:bsl],
-          license_area: transmitter[:license_area],
-          license_id: transmitter[:license_id],
-          operation_hours: transmitter[:operation_hours],
-          status: transmitter[:status]
-        )
-        t.station = station
-        t.save
+        station_data[:transmitters].each do |transmitter|
+          t = Transmitter.find_or_initialize_by(
+            band: transmitter[:band],
+            area: transmitter[:area],
+            callsign: transmitter[:callsign],
+            frequency: transmitter[:frequency],
+            purpose: transmitter[:purpose],
+            polarisation: transmitter[:polarisation],
+            antenna_height: transmitter[:antenna_height],
+            antenna_pattern: transmitter[:antenna_pattern],
+            maximum_erp: transmitter[:maximum_erp],
+            maximum_cmf: transmitter[:maximum_cmf],
+            power: transmitter[:power],
+            technical_specification_number: transmitter[:technical_specification_number],
+            license_number: transmitter[:license_number],
+            site_id: transmitter[:site_id],
+            site_name: transmitter[:site_name],
+            zone: transmitter[:zone],
+            easting: transmitter[:easting],
+            northing: transmitter[:northing],
+            lat: transmitter[:lat],
+            lng: transmitter[:lng],
+            state: transmitter[:state],
+            bsl: transmitter[:bsl],
+            license_area: transmitter[:license_area],
+            license_id: transmitter[:license_id],
+            operation_hours: transmitter[:operation_hours],
+            status: transmitter[:status]
+          )
+          t.station = station
+          t.save
 
-        next if t.persisted?
+          next if t.persisted?
 
-        Rails.logger.info("Data: #{transmitter.inspect}")
-        Rails.logger.info("Errors: #{t.errors.messages}")
+          Rails.logger.info("Data: #{transmitter.inspect}")
+          Rails.logger.info("Errors: #{t.errors.messages}")
+        end
       end
     end
   end
